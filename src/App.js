169911f35d1938f068348cb5c02fb555872bfeb5
Lucifer <rowla101@gmail.com>
2020-01-04 13:55:09 +0100

    added basic components and router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
 
 function App() {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-6">columns number 1</div>
-        <div className="col-6">
-          <span>
-            <i className="fas fa-home"></i>
-          </span>
-        </div>
-      </div>
-    </div>
+    <React.Fragment>
+      <Navbar></Navbar>
+      <Switch>
+        <Route exact path="/" component={ProductList}></Route>
+        <Route path="/details" component={Details}></Route>
+        <Route path="/cart" component={Cart}></Route>
+        <Route component={Default}></Route>
+      </Switch>
+    </React.Fragment>
   );
 }
 
