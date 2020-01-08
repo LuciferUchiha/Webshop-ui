0444e2015b901aa1c45d48a21e0aa250f0dff846
George Rowlands <rowla101@gmail.com>
2020-01-08 22:15:28 +0100

    added card image and started on product component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -33,7 +33,7 @@ export default class Navbar extends Component {
 
 const NavWrapper = styled.nav`
   background: var(--mainBlue);
-  .nav-Link{
+  .nav-Link {
     color: var(--mainWhite) !important;
     font-size: 1.3rem;
   }
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,39 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "../context";
 
 export default class Product extends Component {
   render() {
+    const { id, title, img, price, inCart } = this.props.product;
     return (
-      <div>
-        <h3>Hello from Product</h3>
-      </div>
+      <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+        <div className="card">
+          <div
+            className="img-container p-5"
+            onClick={() => console.log("click on img")}
+          >
+            <Link to="/details">
+              <img src={img} alt="product" className="card-img-top"></img>
+            </Link>
+            <button
+              className="cart-btn"
+              disabled={inCart ? true : false}
+              onClick={() => console.log("added to cart")}
+            >
+              {inCart ? (
+                <p className="mb-0" disabled>
+                  In Cart
+                </p>
+              ) : (
+                <i className="fas fa-cart-plus" />
+              )}
+            </button>
+          </div>
+        </div>
+      </ProductWrapper>
     );
   }
 }
+
+const ProductWrapper = styled.div``;
