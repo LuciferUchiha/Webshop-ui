b535fd7f27afe03075a3208ed7a99a99abdfadbb
George Rowlands <rowla101@gmail.com>
2020-01-20 18:08:45 +0100

    fixed prices

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const storeProducts = [
     id: 1,
     title: "Google Pixel - Black",
     img: "img/product-1.png",
-    price: 10,
+    price: 99,
     company: "GOOGLE",
     info:
       "Get more done with Pixel. It has an extraordinary camera with features like night sight, portrait mode, and HDR+. a battery that charges fast and lasts all day. All the helpfulness of the Google assistant built in. And 3 years of security and os updates included . it’s everything you love about Google – in a phone. Works with all major carriers.",
@@ -15,7 +15,7 @@ export const storeProducts = [
     id: 2,
     title: "Samsung S7",
     img: "img/product-2.png",
-    price: 16,
+    price: 156,
     company: "SAMSUNG",
     info:
       "The Samsung Galaxy S7 edge is more than a smartphone. It's the key that unlocks a galaxy of technology. Big on screen and slim on profile. Less is finally more. The first smartphone with dual-pixel technology features fast autofocus, so you can say goodbye to blurry photos. Catch every detail even in low-light. With its IP68 rating, This device can take on spills and splashes with its premium water-resistant design. Featuring a Quad HD super AMOLED display and is powered by 2.15 GHz/1.6 GHz Quad core Qualcomm Snapdragon 820 processor and runs on Android 6.0.1 (Marshmallow).",
@@ -27,7 +27,7 @@ export const storeProducts = [
     id: 3,
     title: "HTC 10 - Black",
     img: "img/product-3.png",
-    price: 8,
+    price: 310,
     company: "HTC",
     info:
       "Meet HTC 10. The smartphone crafted with obsession. You get unprecedented control. Off the chart performance. And innovations like the world’s first dual OIS for pro level photos front and back, as well as end-to-end 24-bit Hi-Res sound. We’re redefining what a flagship phone is truly meant to be. And think you’ll love the result. It’s the beauty of light, crafted into a brand new all-metal unibody. Every element of HTC 10 has been designed, refined and perfected. From the bold new chamfered contour to its stunning dual-textured finish and quality construction.",
@@ -39,7 +39,7 @@ export const storeProducts = [
     id: 4,
     title: "HTC 10 - White",
     img: "img/product-4.png",
-    price: 18,
+    price: 310,
     company: "HTC",
     info:
       "Meet HTC 10. The smartphone crafted with obsession. You get unprecedented control. Off the chart performance. And innovations like the world’s first dual OIS for pro level photos front and back, as well as end-to-end 24-bit Hi-Res sound. We’re redefining what a flagship phone is truly meant to be. And think you’ll love the result. It’s the beauty of light, crafted into a brand new all-metal unibody. Every element of HTC 10 has been designed, refined and perfected. From the bold new chamfered contour to its stunning dual-textured finish and quality construction.",
@@ -51,7 +51,7 @@ export const storeProducts = [
     id: 5,
     title: "HTC Desire 626s",
     img: "img/product-5.png",
-    price: 24,
+    price: 190,
     company: "HTC",
     info:
       "Get everything you want and more, with a best-in-class 13MP main camera that captures life’s moments in high-resolution. A powerful processor and dual SIM capability keeps you ahead, and a large 5” HD display provides a great viewing experience. Enjoy high-quality performance in a slim, stylish frame.",
@@ -63,7 +63,7 @@ export const storeProducts = [
     id: 6,
     title: "Vintage Iphone",
     img: "img/product-6.png",
-    price: 17,
+    price: 100,
     company: "APPLE",
     info:
       "The iPhone 3GS features iOS, Apple's mobile operating system. The user interface of iOS is based on the concept of direct manipulation, using multi-touch gestures. Interface control elements consist of sliders, switches, and buttons. Interaction with the OS includes gestures such as swipe, tap, pinch, and reverse pinch, all of which have specific definitions within the context of the iOS operating system and its multi-touch interface. Internal accelerometers are used by some applications to respond to shaking the device (one common result is the undo command) or rotating it vertically (one common result is switching from portrait to landscape mode).",
@@ -75,7 +75,7 @@ export const storeProducts = [
     id: 7,
     title: "Iphone 7",
     img: "img/product-7.png",
-    price: 30,
+    price: 200,
     company: "APPLE",
     info:
       "iPhone 7 dramatically improves the most important aspects of the iPhone experience. It introduces advanced new camera systems. The best performance and battery life ever in an iPhone. Immersive stereo speakers. The brightest, most colorful iPhone display. Splash and water resistance. And it looks every bit as powerful as it is. This is iPhone 7. iPhone 7 reaches a new level of innovation and precision. The enclosure is splash and water resistant. The Home button is completely reengineered. And with a new unibody design that's seamless to the touch, iPhone 7 feels as amazing as it looks.",
@@ -87,7 +87,7 @@ export const storeProducts = [
     id: 8,
     title: "Smashed Iphone",
     img: "img/product-8.png",
-    price: 2,
+    price: 50,
     company: "APPLE",
     info:
       "iPhone 7 dramatically improves the most important aspects of the iPhone experience. It introduces advanced new camera systems. The best performance and battery life ever in an iPhone. Immersive stereo speakers. The brightest, most colorful iPhone display. Splash and water resistance. And it looks every bit as powerful as it is. This is iPhone 7. iPhone 7 reaches a new level of innovation and precision. The enclosure is splash and water resistant. The Home button is completely reengineered. And with a new unibody design that's seamless to the touch, iPhone 7 feels as amazing as it looks.",
